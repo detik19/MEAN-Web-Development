942b1c2a77648f81733afaf26fde747df3596276
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-14 14:10:34 +0700

    Reading a single user document using findOne()

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -7,4 +7,13 @@ module.exports= function(app){
 	app.route('/users')
 		.post(users.create)
 		.get(users.list);
+	
+	/*
+	 *  In Express, adding a colon before a substring in a route definition 
+	 *  means that this substring will be handled as a request parameter. 
+	 * 
+	 */
+	app.route('/users/:userId')
+		.get(users.read);
+	app.param('userId', users.userByID);
 };
