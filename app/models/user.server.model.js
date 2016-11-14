3503f220ddce8bf12e97b96b9da0f8c2859649bc
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-14 14:48:38 +0700

    Defning default values

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -8,13 +8,21 @@ var mongoose = require('mongoose'),
 /*
  * defned your UserSchema object using the Schema constructor
  * 
+ * Notice how the created feld is added and its default value defned
+ * 
+ * From now on, every new user document will be created 
+ * with a default creation date that represents the moment the document was created
  */
 var UserSchema= new Schema({
 	firstName: String,
 	lastName: String,
 	email: String,
 	username: String,
-	password: String
+	password: String,
+	created:{
+		type: Date,
+		default: Date.now
+	}
 });
 
 mongoose.model('User', UserSchema);
